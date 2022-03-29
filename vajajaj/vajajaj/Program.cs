c32c7727bb01436d6a81c96f98d0b36da05f2d41
Haris Klepić <klepic.hari@gmail.com>
2022-03-29 09:19:01 +0200

    ponovitev link

--- vajajaj/vajajaj/Program.cs
+++ vajajaj/vajajaj/Program.cs
@@ -10,7 +10,15 @@ namespace vajajaj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("pozdravljen svet!");
+            BazaZaVajeEntities context = new BazaZaVajeEntities();
+            // vsi dobavitelji
+            var x1 = from a in context.DOBAVITELJ
+                     select a;
+            foreach(var y in x1)
+                {
+                Console.WriteLine(y.D_IME+" "+y.D_KONTAKT);
+
+            }
             Console.ReadLine();
         }
     }
